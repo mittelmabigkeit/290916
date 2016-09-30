91bb787c31d5e764c4df780d01b344d9a00745c6
mittelmabigkeit <comp422012016@mail.ru>
2016-09-30 11:05:09 +0300

    homework

--- index.php
+++ index.php
@@ -46,24 +46,24 @@ echo "<br>";
 echo "<br>";
 echo "<br>";
 */
-function printTree($level=l) { 
-$d = @opendir("."); 
-if (!$d) return; 
-while (($e=readdir($d)) !== false) { 
-if ($e=='.' || $e=='..') continue; 
-if (!@is_dir($e)) continue; 
-for ($i=0; $i<$level; $i++) echo " "; 
-echo "$е\n"; 
-if (!chdir($e)) continue; 
-printTree($level+l); 
-chdir(".."); 
-flush();
-} 
-closedir($d); 
-} 
-echo "<рге>"; 
-echo "/\n"; 
-chdir($_SERVER['DOCUMENT_ROOT']); 
-printTree(); 
-echo "</pre>"; 
+function printTree ($level=1){
+     $d=@opendir(".");
+	 if (!$d) return;
+	 while (($e=readdir($d))!==false){
+	 if ($e=='.'|| $e=='..')continue;
+	 if (!@is_dir($e)) continue;
+	 for ($i=0; $i<$level; $i++)echo "  ";
+	 echo "$e\n";
+	 if (!chdir($e)) continue;
+	 printTree($level+1);
+	 chdir("..");
+	 flush();
+	 }
+	 closedir($d);
+}
+echo "<pre>";
+echo "/\n";
+chdir($_SERVER['DOCUMENT_ROOT']);
+PrintTree();
+echo"</pre>"; 
 ?>
